157de396b6a4ad6c3ff47ba98eec543249978ce5
Ahmed <Ahmed@192.168.0.8>
2023-01-30 16:53:43 -0800

    Added Log4j

--- src/main/java/com/iointel/mqtt/mqtt_app/App.java
+++ src/main/java/com/iointel/mqtt/mqtt_app/App.java
@@ -1,5 +1,7 @@
 package com.iointel.mqtt.mqtt_app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -8,52 +10,59 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 
 public class App {
+	
+	// Log4j Logger instantiation
+	private static final Logger logger = LogManager.getLogger();
+	
     public static void main(String[] args) {     
     	String topic        = "MQTT_Examples";
-        String content      = "Message from MqttPublishSample";
+        String content      = "Sample Message";
         int qos             = 0;
         String broker       = "tcp://localhost:1883";
         String clientId     = "MainClient";
         MemoryPersistence persistence = new MemoryPersistence();
         MyMqttCallback cb = new MyMqttCallback();
+        
+        logger.info("hello from App.java");
 
         try {
-        	
-        	// Create Sample Client and connect
+        	// Create Client
             MqttClient client = new MqttClient(broker, clientId, persistence);
-            client.setCallback(cb);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-//            System.out.println("Connecting to broker: "+broker);
-            client.connect(connOpts);
-//            System.out.println("Connected");
             
-            // Subscribe to specified topic
-            // On message received, parse payload and display on console
+            // Set Client Callback functions to custom callback
+            client.setCallback(cb);
+            
+            // Define Connection options
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setAutomaticReconnect(true);
+            
+            // Connect Client to Local Broker
+            client.connect(options);
+            
+            // Subscribe to defined topic
             client.subscribe(topic);
             
-            // Publish a message with specific topic and QoS
-//            System.out.println("Publishing message: "+content);
+            // Create a message and publish with defined topic
             MqttMessage message = new MqttMessage(content.getBytes());
             message.setQos(qos);
             client.publish(topic, message);
-//            System.out.println("Message published");
             
-            // Disconnect Client
+        	// Disconnect Client
             client.disconnect();
-//            System.out.println("Disconnected");
             
             // Close Client
             client.close();
-//            System.out.println("Closed");
+
+            // exit
             System.exit(0);
-        } catch(MqttException me) {
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
-            me.printStackTrace();
+        } catch(MqttException e) {
+        	logger.info("Reason " + e.getReasonCode());
+        	logger.info("Localized Message: " + e.getLocalizedMessage());
+        	logger.info("Cause: " + e.getCause());
+        	logger.info("Exception: " + e);
+        	logger.error("Reason " + e.getReasonCode());
+        	logger.trace(e.getStackTrace());
         }
     }
 }
--- src/main/java/com/iointel/mqtt/mqtt_app/MyMqttCallback.java
+++ src/main/java/com/iointel/mqtt/mqtt_app/MyMqttCallback.java
@@ -1,40 +1,36 @@
 package com.iointel.mqtt.mqtt_app;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.nio.file.attribute.DosFileAttributeView;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MyMqttCallback implements MqttCallback{
+	
+	private static final Logger logger = LogManager.getLogger();
 
 	@Override
 	public void connectionLost(Throwable cause) {
-		// TODO Auto-generated method stub
-		System.out.println("Connect Lost" + cause.getMessage());
+		logger.fatal("Connection Lost");
 	}
 
 	@Override
 	public void messageArrived(String topic, MqttMessage message) throws Exception {
-		if (message == null) {
-			System.out.println("null message received");
+		if (message == null || message.getPayload() == null) {
+			logger.info("Null Message Received");
 			return;
 		}
 		byte[] payload = message.getPayload();
     	String text = new String(payload, "UTF-8");
-    	System.out.println("Subscribed Message: " + text);
+    	logger.info("Subscribed Message: " + text);
     	
     	// Save each message as a separate file in temp/MqttData
     	String dirName = System.getProperty("java.io.tmpdir") + File.separator + "MqttData" + File.separator;
@@ -46,50 +42,47 @@ public class MyMqttCallback implements MqttCallback{
     	}
     	
     	File file = new File(topicDir + File.separator + message.getId() + ".txt");
-//
+
     	file.createNewFile();
-//    	file.setWritable(true);
+    	logger.debug("Message File Created");
     	
     	OutputStream os = null;
     	try {
     		os = new FileOutputStream(file);
     		os.write(payload, 0, payload.length);
     	} catch (IOException e) {
-    		e.printStackTrace();
+    		logger.trace(e.getStackTrace());
     	} finally {
     		try {
     			os.close();
     		} catch (IOException e) {
-    			e.printStackTrace();
+    			logger.trace(e.getStackTrace());
     		}
     	}
+    	
+    	logger.debug("Message Bytes dumped into file: " + file.getName());
     	    	    	
 //    	Files.write(file.toPath(), "hello".getBytes());
 //    	file.setWritable(true, false);
 //    	if (Files.isWritable(file.toPath())) {
 //    		System.out.println("yes");
-//    	}
-    	
-    	
+//    	}    	
 	}
 
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken token){
-//		System.out.println("delivered");
+		logger.debug("Messaged Delivered");
 		MqttMessage message;
 		try {
 			message = token.getMessage();
 			byte[] payload = message.getPayload();
 	    	String text = new String(payload, "UTF-8");
-	    	System.out.println("Published Message: " + text);
+	    	logger.info("Messaged Published: " + text);
 		} catch (MqttException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.trace(e.getStackTrace());
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.trace(e.getStackTrace());
 		}
-		
 	}
 
 }
