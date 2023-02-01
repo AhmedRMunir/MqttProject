a9b98bcbc8007913a10c3412b96766c6ab5f5796
Ahmed <Ahmed@192.168.0.7>
2023-02-01 14:42:16 -0800

    Added a static logger to each class to better identify logs, adjusted some callBacks.

--- src/main/java/com/iointel/mqtt/mqtt_app/App.java
+++ src/main/java/com/iointel/mqtt/mqtt_app/App.java
@@ -1,5 +1,7 @@
 package com.iointel.mqtt.mqtt_app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
@@ -7,23 +9,24 @@ import com.iointel.mqtt.mqtt_app.utilities.CloseUtility;
 import com.iointel.mqtt.mqtt_app.utilities.MqttUtility;
 
 public class App {
+	
+	private static String content = "Sample Message";
+	private static final Logger logger = LogManager.getLogger(CloseUtility.class);
 
 	public static void main(String[] args) throws MqttAppException {
-		Constants.Init.logger.info("App Start");
-		MqttClient client = null;
+		MqttClient client;
 		try {
-			client = MqttUtility.createClient(Constants.Init.broker, Constants.Init.clientId, Cache.persistence);
+			client = MqttUtility.createClient(Constants.Init.broker, Constants.Init.clientId);
 			MqttUtility.connectClient(client, Cache.callback);
 			MqttUtility.subscribeTopic(client, Constants.Init.topic);
-			String content = "Sample Message";
+			
 			MqttMessage message = MqttUtility.createMessage(content, Constants.Init.qos);
 			MqttUtility.publishMessage(client, Constants.Init.topic, message);
-		} catch (MqttAppException e) {
-			Constants.Init.logger.error(e);
-		} finally {
 			MqttUtility.disconnectClient(client);
-//			MqttUtility.closeClient(client);
 			CloseUtility.close(client);
+		} catch (MqttAppException e) {
+			logger.error(e);
+		} finally {
 		}
 	}
 }
